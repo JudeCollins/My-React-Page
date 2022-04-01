87e28acce02a92a2898ccc85e8d46c2b3bea03a3
Jude Collins <jude.collins13@yahoo.com>
2022-04-01 13:18:54 -0400

    fix

--- src/data.js
+++ src/data.js
@@ -7,26 +7,29 @@ export const products = [
     {
       id: 2,
       img: "",
-      link: "http://lama.dev",
+      link: "https://drive.google.com/file/d/1FbteLDJ0P8E1euMoK50ho3XUPsGPrmBh/view?usp=sharing",
     },
     {
       id: 3,
       img: "",
-      link: "http://lama.dev",
+      link: "https://drive.google.com/file/d/1FbteLDJ0P8E1euMoK50ho3XUPsGPrmBh/view?usp=sharing",
     },
     {
       id: 4,
       img: "",
-      link: "http://lama.dev",
+      link: "https://drive.google.com/file/d/1FbteLDJ0P8E1euMoK50ho3XUPsGPrmBh/view?usp=sharing",
     },
     {
       id: 5,
       img: "",
-      link: "http://lama.dev",
+      link: "https://drive.google.com/file/d/1FbteLDJ0P8E1euMoK50ho3XUPsGPrmBh/view?usp=sharing",
     },
     {
       id: 6,
       img: "",
-      link: "http://lama.dev",
+      link: "https://drive.google.com/file/d/1FbteLDJ0P8E1euMoK50ho3XUPsGPrmBh/view?usp=sharing",
     },
   ];
+
+
+  
